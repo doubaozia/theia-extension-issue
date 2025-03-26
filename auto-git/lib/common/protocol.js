fde5a816b6ad65d9c027157723afc360a515c5f9
zhangdoujun <zhangdj@heywhale.com>
2025-03-26 20:50:24 +0800

    feat: fixed scripts

--- auto-git/lib/common/protocol.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AutoGitBackendService = void 0;
-exports.AutoGitBackendService = Symbol('AutoGitBackendService');
-;
-//# sourceMappingURL=protocol.js.map
--- auto-git/lib/node/utils.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function execShell(sh: string, cwd?: string): Promise<string>;
-export declare function writeSSHKey(content: string): void;
-export declare function getProjectDir(): string;
-export declare function getLocalGitDir(gitUrl: string): string;
-export declare function getConfigFilePath(): string;
-export declare function getDefaultWorkspace(): string;
-//# sourceMappingURL=utils.d.ts.map
